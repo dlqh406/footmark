bfe960d87cb157bd1bdb54b43309b43e54efd2ff
Lee bosung <dlqh406@gmail.com>
2020-09-16 01:08:49 +0900

    form  등록완료 : 스프레드시트 재설정예정 // 약관 피시에서 안보이게 설정 // 폰트크기변경

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,12 +1,14 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
-   apiKey: "AIzaSyDaivfsQqIiqbxPWCIiXDfYN6vQUbwxtz0",
-   authDomain: "final2-bal.firebaseapp.com",
-   databaseURL: "https://final2-bal.firebaseio.com",
-   projectId: "final2-bal",
-   storageBucket: "final2-bal.appspot.com",
-   messagingSenderId: "432346550801",
-   appId: "1:432346550801:web:c0ea0a2ac019ed1c9297b0"
- };
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//   apiKey: "AIzaSyC-1s7zNQdq1OoBhJxM0Wq2EnBdqw75XwY",
+//   authDomain: "balbadoc.firebaseapp.com",
+//   databaseURL: "https://balbadoc.firebaseio.com",
+//   projectId: "balbadoc",
+//   storageBucket: "balbadoc.appspot.com",
+//   messagingSenderId: "445909321883",
+//   appId: "1:445909321883:web:104d48724df0762be81e3e",
+//   measurementId: "G-1044DEQ7X2",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
